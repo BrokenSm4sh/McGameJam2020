4afffd8efff2e342e8c98b40acbf77ef8add9a41
BrokenSm4sh <charles.sterlin@hotmail.com>
2020-01-11 13:30:08 -0500

    Charles scene

---
+++ McGamejam2020/Assets/Scripts/CameraMain.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraMain : MonoBehaviour
+{
+    public GameObject subject;
+
+    private Vector3 offset;            //Private variable to store the offset distance between the player and camera
+
+    private bool up = true;
+    private float threshold = 0;
+
+    // Use this for initialization
+    void Start()
+    {
+        //Calculate and store the offset value by getting the distance between the player's position and camera's position.
+        offset = transform.position - subject.transform.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // Set the position of the camera's transform to be the same as the player's, but offset by the calculated offset distance.
+
+        if (Input.GetKey(KeyCode.LeftArrow))
+        {
+            //transform.RotateAround(subject.gameObject.transform.position,new Vector3(0,1,0),1f);
+            transform.RotateAround(subject.transform.position, new Vector3(0, 0.5f, 0), 1);
+
+        }
+        if (Input.GetKey(KeyCode.RightArrow))
+        {
+            //transform.RotateAround(subject.gameObject.transform.position,new Vector3(0,1,0),1f);
+            transform.RotateAround(subject.transform.position, new Vector3(0, -0.5f, 0), 1);
+        }
+        if (Input.GetKey(KeyCode.DownArrow))
+        {
+            //transform.RotateAround(subject.gameObject.transform.position,new Vector3(0,1,0),1f);
+           // transform.RotateAround(subject.transform.position, new Vector3(0, 0, -0.5f), 1);
+            transform.Translate(new Vector3(0, -0.5f,0));
+        }
+        if (Input.GetKey(KeyCode.UpArrow))
+        {
+            //transform.RotateAround(subject.gameObject.transform.position,new Vector3(0,1,0),1f);
+            // transform.RotateAround(subject.transform.position, new Vector3(0, 0, 0.5f), 1);
+            transform.Translate(new Vector3(0, 0.5f,0));
+        }
+        //else
+        //{
+        //    transform.position = subject.transform.position + offset;
+        //}
+        transform.LookAt(subject.transform, Vector3.up);
+
+    }
+}
---
+++ McGamejam2020/Assets/Scripts/MoveMockup.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveMockup : MonoBehaviour
+{
+    [SerializeField]
+    private Rigidbody rgbd;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKey(KeyCode.W))
+        {
+            Debug.Log("w");
+            rgbd.velocity += Camera.main.transform.forward;
+        }
+        if (Input.GetKey(KeyCode.A))
+        {
+            Debug.Log("a");
+            rgbd.velocity += -Camera.main.transform.right;
+        }
+        if (Input.GetKey(KeyCode.S))
+        {
+            Debug.Log("s");
+            rgbd.velocity +=  -Camera.main.transform.forward;
+        }
+        if (Input.GetKey(KeyCode.D))
+        {
+            Debug.Log("d");
+            rgbd.velocity += Camera.main.transform.right;
+        }
+    }
+}
