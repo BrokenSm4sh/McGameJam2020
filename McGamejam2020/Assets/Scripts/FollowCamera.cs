38b264168154576981016e0d2819e20b63c64726
Buzzzzzz <31232553+buzzzz0505@users.noreply.github.com>
2020-01-11 13:59:38 -0500

    Update with Asset

---
+++ McGamejam2020/Assets/Scripts/FollowCamera.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FollowCamera : MonoBehaviour {
+    // fields for camera movement;
+    public GameObject player;
+
+    Transform cameraTrfm;
+    Transform playerTrfm;
+
+    Vector3 offset = new Vector3();
+
+    // Start is called before the first frame update
+    void Start() {
+        // get references
+        playerTrfm = player.GetComponent<Transform>();
+
+        cameraTrfm = GetComponent<Transform>();
+
+        // get initial offset
+        offset = cameraTrfm.position - player.transform.position;
+    }
+
+    // Update is called once per frame
+    void Update() {
+
+    }
+
+    private void LateUpdate() {
+        cameraTrfm.position = player.transform.position + offset;
+    }
+}
---
+++ McGamejam2020/Assets/Scripts/UIController.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UIController : MonoBehaviour
+{
+    private GameObject camera;
+    private GameObject player;
+    private GameObject nearbyPrompt;
+    private GameObject positionInticator;
+
+    private bool isPickedUp = false;
+
+    public float pickUpAvailableDistance;
+    // Start is called before the first frame update
+    void Start()
+    {
+        camera = GameObject.FindGameObjectWithTag("MainCamera");
+        player = GameObject.FindGameObjectWithTag("Player");
+
+        nearbyPrompt = gameObject.transform.GetChild(0).gameObject;
+        positionInticator = gameObject.transform.GetChild(1).gameObject;
+
+        // reference event camera
+        gameObject.GetComponent<Canvas>().worldCamera =
+            GameObject.FindGameObjectWithTag("MainCamera").GetComponent<Camera>();
+
+        // register events
+        PlayerController.onPickUp += onCollecPickedUp;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.rotation = Quaternion.LookRotation(transform.position - camera.transform.position);
+
+        // check if player within proximity
+        if (Vector3.Distance(player.transform.position, gameObject.transform.position) <= pickUpAvailableDistance
+            && !isPickedUp) {
+            // set nearbyPrompt  to be active
+            nearbyPrompt.GetComponent<Text>().text = "E to Pick Up";
+            nearbyPrompt.SetActive(true);
+            positionInticator.SetActive(false);
+
+        } else if (!isPickedUp) {
+            // set position indicator to be active 
+            nearbyPrompt.SetActive(false);
+            positionInticator.SetActive(true);
+        }
+    }
+
+    void onCollecPickedUp() {
+        isPickedUp = true;
+        nearbyPrompt.GetComponent<Text>().text = "R to put down";
+    }
+
+    private void OnDisable() {
+        PlayerController.onPickUp -= onCollecPickedUp;
+    }
+}
