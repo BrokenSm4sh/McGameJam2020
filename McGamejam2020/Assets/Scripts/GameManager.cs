d79049ba5d429f3d35a46333a62caa18aac44c56
BrokenSm4sh <charles.sterlin@hotmail.com>
2020-01-11 18:16:42 -0500

    a

--- McGamejam2020/Assets/Scripts/GameManager.cs
+++ McGamejam2020/Assets/Scripts/GameManager.cs
@@ -13,6 +13,7 @@ public class GameManager : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
+        AppManager.SetGameInstance(this);
         isPaused = false;
     }
 
