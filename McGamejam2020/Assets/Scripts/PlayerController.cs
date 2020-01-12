b7d3d6129dff717f10d59b2ac51f54117d1c03cb
Buzzzzzz <31232553+buzzzz0505@users.noreply.github.com>
2020-01-12 04:10:04 -0500

    trying to pull

--- McGamejam2020/Assets/Scripts/PlayerController.cs
+++ McGamejam2020/Assets/Scripts/PlayerController.cs
@@ -13,9 +13,9 @@ public class PlayerController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.E)) {
-            GameObject.FindGameObjectWithTag("okToPickUp")
-                .GetComponent<CollectableController>().pickUp();
+        CollectableController CollectableCon = GameObject.FindGameObjectWithTag("okToPickUp").GetComponent<CollectableController>();
+        if (Input.GetKeyDown(KeyCode.E) && CollectableCon != null) {
+            CollectableCon.pickUp();
         }
     }
 }
