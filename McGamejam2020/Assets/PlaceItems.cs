6f1735839a1c286825dc515771cd0117b0c884a2
BrokenSm4sh <charles.sterlin@hotmail.com>
2020-01-12 10:32:01 -0500

    camera

--- McGamejam2020/Assets/PlaceItems.cs
+++ McGamejam2020/Assets/PlaceItems.cs
@@ -18,19 +18,51 @@ public class PlaceItems : MonoBehaviour
     // Start is called before the first frame update
     void Awake()
     {
+        List<KeyValuePair<float,float>> positions = new List<KeyValuePair<float,float>>();
+        float generatedX;
+        float generatedZ;
         int i = 0;
+        int noLuck = 0;
         foreach (var obj in objectsToPlace)
         {
             i = 0;
             while(i < obj.amountPerObject)
             {
-                float generatedX = Random.Range(0,10);
-                float generatedZ = Random.Range(0,10);
+                KeyValuePair<float, float> currentPos;
+                noLuck = 0;
+                do
+                {
+                     generatedX = Random.Range(0, 1000);
+                     generatedZ = Random.Range(0, 1000);
+                    currentPos = new KeyValuePair<float, float>(generatedX, generatedZ);
+                    noLuck++;
+                } while (!NotInRange(currentPos, positions) || noLuck < 255);
+
+                // if (noLuck == 255)
+                // {
+                //     Debug.Log("no luck :(");
+                //     continue;
+                // }
+                positions.Add(currentPos);
                 Vector3 generatedPosition = new Vector3(generatedX,0, generatedZ);
-                if (obj.objectToPlace != null) Object.Instantiate<GameObject>(obj.objectToPlace, generatedPosition,new Quaternion());
+                if (obj.objectToPlace != null) Object.Instantiate<GameObject>(obj.objectToPlace, generatedPosition,new Quaternion()).SetActive(true);
                 i++;
             }
         }
+    }
+
+    private bool NotInRange(KeyValuePair<float, float> currentPos, List<KeyValuePair<float, float>> positions)
+    {
+        foreach (var position in positions)
+        {
+            if (Mathf.Abs(position.Key - currentPos.Key) < 10 ||
+                Mathf.Abs(position.Value - currentPos.Value) < 10)
+            {
+                return false;
+            }
+        }
+
+        return true;
     }
 
     // Update is called once per frame
