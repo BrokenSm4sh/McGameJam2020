52e2aeac0a85cbcb567d39a44f975f8c994c8bd9
BrokenSm4sh <charles.sterlin@hotmail.com>
2020-01-12 13:47:27 -0500

    Revert "Revert "Merge branch 'master' of https://github.com/BrokenSm4sh/McGameJam2020""
    This reverts commit 0d53f8609d46ac815b0121760c9ac7573e773be3.

--- McGamejam2020/Assets/Scripts/GameManager.cs
+++ McGamejam2020/Assets/Scripts/GameManager.cs
@@ -1,42 +1,73 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+using UnityEngine.UIElements;
+using Image = UnityEngine.UI.Image;
 
 public class GameManager : MonoBehaviour
 {
-    [SerializeField]
-    private  Canvas gameOverScreen;
+    [SerializeField] private Canvas gameOverScreen;
+
+    [SerializeField] private RawImage startScreen;
+    [SerializeField] public Texture story1;
+    [SerializeField] public Texture story2;
+    [SerializeField] public Texture story3;
+    [SerializeField] public Texture story4;
+
+    private Texture currentImage;
+
+    private List<Texture> storyBoard;
 
     private bool isPaused;
-    private ControllerStuff controls; 
-    
+    private bool storyStarted;
+    private ControllerStuff controls;
+
+
     // Start is called before the first frame update
     void Awake()
     {
+        storyBoard = new List<Texture>();
+        storyStarted = false;
         controls = new ControllerStuff();
         controls.Menu.Pause.performed += ctx => ToggleGame();
+        controls.Menu.StartSotryboard.performed += ctx => ContinueStory();
         AppManager.SetGameInstance(this);
-        isPaused = false;
+        PauseGame();
+        startScreen.enabled = true;
+    }
+
+    private void Start()
+    {
+        storyBoard.AddRange(new[] {story1, story2, story3, story4});
+    }
+
+    private void ContinueStory()
+    {
+        var curIndex = storyBoard.IndexOf(currentImage)+1;
+        if (curIndex < storyBoard.Count)
+        {
+            currentImage = storyBoard[storyBoard.IndexOf(currentImage) + 1];
+            startScreen.texture = currentImage;
+        }
+        else
+        {
+            startScreen.enabled = false;
+            gameOverScreen.enabled = false;
+            controls.Menu.StartSotryboard.performed -= ctx => ContinueStory();
+            ContinueGame();
+
+        }
     }
 
     // Update is called once per frame
     void Update()
     {
-        if(Input.GetKey(KeyCode.R))
-        {
-            hardRestartGame();
-        }
-        if(Input.GetKey(KeyCode.G))
-        {
-            ForceGameOver();
-        }
-        if(Input.GetKeyDown(KeyCode.P))
-        {
-            ToggleGame();
-        }
+       
     }
-    
+
     private void OnEnable()
     {
         controls.Menu.Enable();
@@ -66,9 +97,16 @@ public class GameManager : MonoBehaviour
         isPaused = true;
         //pausePanel.SetActive(true);
         //Disable scripts that still work while timescale is set to 0
-    } 
+    }
+
     private void ContinueGame()
     {
+        if (startScreen.IsActive())
+        {
+            storyStarted = true;
+            currentImage = story1;
+        }
+
         Time.timeScale = 1;
         isPaused = false;
         //pausePanel.SetActive(false);
@@ -79,7 +117,6 @@ public class GameManager : MonoBehaviour
     {
         SceneManager.LoadScene(SceneManager.GetActiveScene().name);
         gameOverScreen.gameObject.SetActive(false);
-
     }
 
     public void ForceGameOver()
