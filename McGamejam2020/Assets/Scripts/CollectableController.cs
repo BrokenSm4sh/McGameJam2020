3cbeef5965249492f0a968015803c4663c4b8d3f
BrokenSm4sh <charles.sterlin@hotmail.com>
2020-01-11 20:21:47 -0500

    b

--- McGamejam2020/Assets/Scripts/CollectableController.cs
+++ McGamejam2020/Assets/Scripts/CollectableController.cs
@@ -1,62 +1,48 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.UI;
 
 public class CollectableController : MonoBehaviour
 {
     public GameObject player;
     public float CollectableZOffsetFromPlayer;
 
-    public float PICK_UP_RADIUS;
-
     public float pickUpUIYOffset;
-    public GameObject pickupUIPrefab;
-    private GameObject pickupUIInstance;
-
+    public GameObject pickupUI;
 
     // Start is called before the first frame update
     void Start()
     {
         // spawn pickUp UI
-        pickupUIInstance = Instantiate(pickupUIPrefab,
-            new Vector3(transform.position.x, 
-            transform.position.y + pickUpUIYOffset, 
-            transform.position.z), 
-            Quaternion.identity,
-            gameObject.transform);
+        Instantiate(pickupUI,
+            new Vector3(transform.position.x, transform.position.y + pickUpUIYOffset, transform.position.z), Quaternion.identity)
+            .transform.SetParent(gameObject.transform);
+
+        // register event listener
+        PlayerController.onPickUp += OnCollecPickedUp;
 
     }
 
     // Update is called once per frame
     void Update()
     {
-        // if player within pick up radius, change tag to okToPickUp
-        if (Vector3.Distance(gameObject.transform.position, player.transform.position) <= PICK_UP_RADIUS) {
-            if (gameObject.tag != "pickedUpCollectable") {
-                gameObject.tag = "okToPickUp";
-            }
-            
-        } 
+        
     }
 
     // handle collision with player
-    public void pickUp() {
+    private void OnCollecPickedUp() {
         gameObject.transform.SetParent(player.transform);
         gameObject.transform.position = new Vector3(
             player.transform.position.x,
             player.transform.position.y,
             player.transform.position.z + CollectableZOffsetFromPlayer);
-        gameObject.tag = "pickedUpCollectable";
-        pickupUIInstance.GetComponent<CollectableUIController>().isPickedUp = true;
-        pickupUIInstance.transform.GetChild(0).gameObject
-            .GetComponent<Text>().text = " ";
     }
 
-    public void drop(Vector3 dropOffLocation) {
-        gameObject.transform.parent = null;
-        gameObject.transform.position = dropOffLocation;
-        pickupUIInstance.transform.GetChild(0).gameObject
-            .GetComponent<Text>().text = " ";
+    private void OnCollecPutDown() {
+        transform.parent = null;
+    }
+
+    private void OnDisable() {
+        PlayerController.onPickUp -= OnCollecPickedUp;
     }
 }
